8cbfe9fd361445f9b82e8c228417b4f74828241f
princesinghkushwah <kushwahprince45@gmail.com>
2023-03-23 11:18:34 +0530

    commit

---
+++ src/app/head.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Head = () => {
+    return (
+        <>
+            <title>
+                IMDB
+            </title>
+            <meta name="description" content='this is imdb clone' />
+        </>
+    )
+}
+
+export default Head
