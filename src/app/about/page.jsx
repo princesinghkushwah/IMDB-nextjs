16593db4083ae477b0a76cd81e3378bdf8b522c2
princesinghkushwah <kushwahprince45@gmail.com>
2023-03-24 17:12:17 +0530

    create Navbar

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div className='max-w-6xl mx-auto space-y-4'>
-            <h1 className='text-2xl font-medium text-amber-600'>
-                About
-            </h1>
-            <p>IMDb an acronym for Internet Movie Databaseis an online database of information related to films, television series, podcasts, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews. IMDb began as a fan-operated movie database on the Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993. Since 1998, it is now owned and operated by IMDb.com, Inc., a subsidiary of Amazon.
-            </p>
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        IMDb an acronym for Internet Movie Databaseis an online database of
+        information related to films, television series, podcasts, home videos,
+        video games, and streaming content online – including cast, production
+        crew and personal biographies, plot summaries, trivia, ratings, and fan
+        and critical reviews. IMDb began as a fan-operated movie database on the
+        Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993.
+        Since 1998, it is now owned and operated by IMDb.com, Inc., a subsidiary
+        of Amazon.
+      </p>
 
-            <p>As of March 2022, the database contained some 10.1 million titles including television episodes and 11.5 million person records.[3] Additionally, the site had 83 million registered users. The site's message boards were disabled in February 2017.
-            </p>
+      <p>
+        As of March 2022, the database contained some 10.1 million titles
+        including television episodes and 11.5 million person records.[3]
+        Additionally, the site had 83 million registered users. The site's
+        message boards were disabled in February 2017.
+      </p>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default About
+export default About;
