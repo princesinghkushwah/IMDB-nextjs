0e036018d47ec172fdfe12b47a76a61e3475efa9
princesinghkushwah <kushwahprince45@gmail.com>
2023-03-24 18:33:19 +0530

    add loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center">
+      {" "}
+      <img className="h-96" src="" alt="loading" />
+    </div>
+  );
+};
+
+export default loading;
